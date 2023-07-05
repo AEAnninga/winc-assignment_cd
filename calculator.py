e09b8973ff8a20e78c2d2db17412a1e677454d4a
Aris Anninga <88113610+AEAnninga@users.noreply.github.com>
2023-07-05 21:24:28 +0200

    changed background color

--- calculator.py
+++ calculator.py
@@ -1,26 +1,9 @@
 from flask import Flask, render_template, request, url_for
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+# from flask_sqlalchemy import SQLAlchemy
+# from datetime import datetime
 from util import calculate_input
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///calculator.db'
-db = SQLAlchemy(app)
-# db.init_app(app)
-
-
-class Calculator(db.Model):
-    id = db.Column(db.Integer, primary_key=True,)
-    calculation_string = db.Column(db.String(200), nullable=False)
-    calculation_result = db.Column(db.Float, default=0)
-    first_number = db.Column(db.Float, default=0)
-    second_number = db.Column(db.Float, default=0)
-
-    def __repr__(self):
-        return '<Task %r>' % self.id
-
-with app.app_context():
-    db.create_all()
 
 @app.route("/", methods=['POST','GET'])
 def calculator(result=None):
